65bb93407d5d0d7729685acf86be7f9a0210c0db
yyejin00 <bywlslgnsl20@gmail.com>
2026-01-28 23:54:03 +0900

    feat: 스터디 상세 patch 검증스키마 작성,delete api 수정

--- src/schemas/study.schema.js
+++ src/schemas/study.schema.js
@@ -43,3 +43,37 @@ export const studyIdParamSchema = z.object({
     .length(26, { message: 'ID는 26자의 ULID 형식이어야 합니다.' })
     .regex(/^[0-9A-HJKMNP-TV-Z]{26}$/i, { message:STUDY_ERROR_MESSAGES.STUDY_INVALID}),
 });
+
+  export const updateStudySchema = z.object({
+  nickname: z
+    .string()
+    .trim()
+    .min(1, STUDY_ERROR_MESSAGES.NICKNAME_INVALID)
+    .max(STUDY_LIMITS.NICKNAME.MAX_LENGTH, STUDY_ERROR_MESSAGES.NICKNAME_INVALID),
+  title: z
+    .string()
+    .trim()
+    .min(1, STUDY_ERROR_MESSAGES.TITLE_INVALID)
+    .max(STUDY_LIMITS.TITLE.MAX_LENGTH, STUDY_ERROR_MESSAGES.TITLE_INVALID),
+  introduction: z
+    .string()
+    .min(1, STUDY_ERROR_MESSAGES.INTRODUCTION_INVALID)
+    .max(STUDY_LIMITS.INTRODUCTION.MAX_LENGTH, STUDY_ERROR_MESSAGES.INTRODUCTION_INVALID),
+  background: z
+    .string()
+    .refine((val) => ALLOWED_BACKGROUND_PATHS.includes(val), {
+      message: STUDY_ERROR_MESSAGES.BACKGROUND_INVALID,
+    }),
+  password: z
+    .string({ required_error: STUDY_ERROR_MESSAGES.PASSWORD_INVALID })
+    .trim()
+    .min(STUDY_LIMITS.PASSWORD.MIN_LENGTH)
+    .max(STUDY_LIMITS.PASSWORD.MAX_LENGTH)
+    .regex(REGEX.NO_SPACE, STUDY_ERROR_MESSAGES.PASSWORD_NO_SPACE),
+}).partial({
+  // 이 아래 필드들은 선택사항(보낼 수도 있고 안 보낼 수도 있음)
+  nickname: true,
+  title: true,
+  introduction: true,
+  background: true,
+});
