dae4f78b1743850a69d1aaf45ce5503860a3a453
lareina7486 <lareina7486@gmail.com>
2026-01-29 18:18:33 +0900

    fix: export 관련 에러 수정

--- src/exceptions/badRequestException.js
+++ src/exceptions/badRequestException.js
@@ -1,7 +1,7 @@
 import { HttpException } from './httpException.js';
 import { ERROR_MESSAGES } from '#constants';
 
-export class badRequestException extends HttpException {
+export class BadRequestException extends HttpException {
   constructor(message = ERROR_MESSAGES.BAD_REQUEST, detailes = null) {
     super(400, message, details);
   }
