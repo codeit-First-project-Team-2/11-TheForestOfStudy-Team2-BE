a4b781e1fc157428732ba2a8f37f253f95dac860
lareina7486 <lareina7486@gmail.com>
2026-01-29 18:42:22 +0900

    fix: HTTP 상태 코드 매직 넘버 수정

--- src/exceptions/conflictException.js
+++ src/exceptions/conflictException.js
@@ -1,8 +1,9 @@
 import { HttpException } from './httpException.js';
 import { ERROR_MESSAGES } from '#constants';
+import { HTTP_STATUS } from '#constants';
 
 export class ConflictException extends HttpException {
   constructor(message = ERROR_MESSAGES.RESOURCE_CONFLICT, detailes = null) {
-    super(409, message, details);
+    super(HTTP_STATUS.CONFLICT, message, details);
   }
 }
