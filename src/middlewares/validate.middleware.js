0294ccc4687580c7f695b9694293a0df67d7b1f9
Minsung Kim <alstjddl0513@gmail.com>
2026-01-29 14:11:21 +0900

    fix: 에러상수 오타 수정

--- src/middlewares/validate.middleware.js
+++ src/middlewares/validate.middleware.js
@@ -1,7 +1,7 @@
 import { isProduction } from '#config';
 import { flattenError } from 'zod';
 import { BadRequestException } from '#exceptions';
-import { ERROR_MESSAGE } from '#constants'; //ERROR_MESSAGES 로 바꿔야 할 것 같아요!
+import { ERROR_MESSAGES } from '#constants'; 
 
 export const validate = (target, schema) => {
   if (!['body', 'query', 'params'].includes(target)) {
@@ -18,11 +18,11 @@ export const validate = (target, schema) => {
         const { fieldErrors } = flattenError(result.error);
 
         if (isProduction) {
-          throw new BadRequestException(ERROR_MESSAGE.INVALID_INPUT);
+          throw new BadRequestException(ERROR_MESSAGES.INVALID_INPUT);
         }
 
         throw new BadRequestException(
-          ERROR_MESSAGE.VALIDATION_FAILED,
+          ERROR_MESSAGES.VALIDATION_FAILED,
           fieldErrors,
         );
       }
