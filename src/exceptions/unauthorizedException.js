fc39a885d018c70d544ed7542dff141c446f40cb
lareina7486 <lareina7486@gmail.com>
2026-02-04 12:08:06 +0900

    fix: details 오타 수정

--- src/exceptions/unauthorizedException.js
+++ src/exceptions/unauthorizedException.js
@@ -3,7 +3,7 @@ import { ERROR_MESSAGES } from '#constants';
 import { HTTP_STATUS } from '#constants';
 
 export class UnauthorizedException extends HttpException {
-  constructor(message = ERROR_MESSAGES.AUTH_FAILED, detailes = null) {
+  constructor(message = ERROR_MESSAGES.AUTH_FAILED, details = null) {
     super(HTTP_STATUS.UNAUTHORIZED, message, details);
   }
 }
