894c26e863801f78d54c6eac1f9f834b2a4d032a
lareina7486 <lareina7486@gmail.com>
2026-02-04 12:14:09 +0900

    fix: 안쓰는 인자 _ 처리

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,4 +1,4 @@
-export const errorHandler = (err, req, res, next) => {
+export const errorHandler = (err, req, res, _next) => {
   // HttpException에서 던진 status가 없으면 기본 500을 사용
   const status = err.status || 500;
   const message = err.message || 'Internal Server Error';
