f85a633d4388fac76ed040f16a12544acbfb9cc9
lareina7486 <lareina7486@gmail.com>
2026-02-04 12:18:01 +0900

    feat: home 라우터 업데이트

--- src/routes/home/home.route.js
+++ src/routes/home/home.route.js
@@ -1,31 +1,64 @@
-import { EMOJI_LIMITS } from '../../constants/validation.constant.js';
 import express from 'express';
-const homeRouter = express.Router;
+import { validate } from '#middlewares/validate.middleware.js';
+import { homeRepository } from '../../repositories/home.repository.js';
+import { HTTP_STATUS } from '#constants';
+import { homePageSchema } from './home.schema.js';
 
-homeRouter.get('/', async (req, res) => {
-  const { page = 1, limit = 6, keyword = '', sort } = req.query;
-  try {
-    // 페이지네이션
-    const currentPage = parseInt(page);
-    const limitNumber = parseInt(limit);
-    const skip = (currentPage - 1) * limitNumber;
-    // sort
-    if (sort === 'points') {
-      filtered.sort((a, b) => b.totalPoint - a.totalPoint);
-    } else {
-      filtered.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+const homeRouter = express.Router();
+
+// 담당: 김민성
+homeRouter.get(
+  '/',
+  validate('query', homePageSchema),
+  async (req, res, next) => {
+    try {
+      const { sort, page, limit, keyword } = req.query;
+
+      //페이지네이션 로직
+      const skip = (page - 1) * limit;
+
+      //정렬 로직 ( 기준:  key = [totalPoint, createdAt, updatedAt] )
+      const orderBy = sort
+        ? sort.split(',').map((key) => {
+            if (key.startsWith('-')) {
+              return { [key.substring(1)]: 'desc' };
+            } else {
+              return { [key]: 'asc' };
+            }
+          })
+        : [{ createdAt: 'desc' }];
+
+      //검색 로직
+      const hasKeyword = keyword !== '';
+      const where = hasKeyword
+        ? {
+            OR: [
+              { title: { contains: keyword, mode: 'insensitive' } },
+              { introduction: { contains: keyword, mode: 'insensitive' } },
+            ],
+          }
+        : {};
+
+      //레포지토리 적용
+      const [studies, totalCount] = await homeRepository.getStudyLists({
+        where,
+        orderBy,
+        skip,
+        take: limit,
+      });
+
+      const totalPage = Math.max(1, Math.ceil(totalCount / limit));
+
+      return res.status(HTTP_STATUS.OK).json({
+        totalCount,
+        totalPage,
+        currentPage: page,
+        data: studies,
+      });
+    } catch (error) {
+      next(error);
     }
-    // 필터링
-    const filtered = allstudies.filter((study) =>
-      study.title.includes(keyword),
-    );
-    // 전체갯수
-    const totalpage = Math.ceil(totalCount);
-    // 응답
-    return res.status(200).json({
-      totalpage,
-      currentPage,
-      totalCount,
-    });
-  } catch (error) {}
-});
+  },
+);
+
+export default homeRouter;
